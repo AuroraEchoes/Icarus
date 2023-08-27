0e75c9736300c91acb30c52f1b9b7cc6e19bffa3
AuroraEchoes <auroraesmerelda@gmail.com>
2023-08-27 12:48:28 +1000

    Rename `ev3-lang-rust-template` to `icarus`

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Building!"
 cargo build --release
 echo "Sending!"
-sshpass -p "maker" scp ~/Projects/CompanyLame/ev3dev-lang-rust-template/target/armv5te-unknown-linux-musleabi/release/ev3dev-lang-rust-template robot@10.0.0.214:/home/robot/
+sshpass -p "maker" scp ~/Projects/CompanyLame/icarus/target/armv5te-unknown-linux-musleabi/release/icarus robot@10.0.0.214:/home/robot/
 echo "Connecting!"
-sshpass -p "maker" ssh -o StrictHostKeyChecking=no robot@10.0.0.214 "export RUST_BACKTRACE=full && ./ev3dev-lang-rust-template"
+sshpass -p "maker" ssh -o StrictHostKeyChecking=no robot@10.0.0.214 "export RUST_BACKTRACE=full && ./icarus"
